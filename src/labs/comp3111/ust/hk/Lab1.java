7a550873e06f49e56b52ce80b637d1200e4c8eec
User1 <User1@user>
2018-02-12 17:32:35 +0800

    Added a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -4,5 +4,6 @@ public class Lab1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Lab1: Introduction to Git and Github");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 }
